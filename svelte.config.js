cdcc2b1c43802ad28e9394f5087597ed8d27abd8
Johnny Xmas <johnny+github@johnnyxmas.net>
2025-08-04 10:31:43 -0500

    resolve deployment problems

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,13 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '' : ''
+		}
 	}
 };
 
